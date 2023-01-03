7a9ebcf2657d903ca5ef772dbe459db9923cc29d
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2023-01-03 14:54:02 -0700

    updated code

--- lambda_function.py
+++ lambda_function.py
@@ -1,22 +1,24 @@
 import boto3
 
-def lambda_handler(event, context):
+def handler(event, context):
     # Connect to DynamoDB
     dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table("visitors")
-    
+    table = dynamodb.Table("my-resume-website-table")
+
     # Get the current visit count
-    response = table.get_item(Key={"pk": 1, "sk": 1})
+    response = table.get_item(Key={"pk": "1", "sk": "1"})
     item = response.get("Item", {})
     visit_count = item.get("visit_count", 0)
-    
+
     # Increment the visit count
     visit_count += 1
-    
+
     # Update the visit count in DynamoDB
-    table.put_item(Item={"pk": 1, "sk": 1, "visit_count": visit_count})
+    table.put_item(Item={"pk": "1", "sk": "1", "visit_count": visit_count})
+
+   # Return the updated visit count
+    # Update the visit count in DynamoDB
+    table.put_item(Item={"pk": "1", "sk": "1", "visit_count": visit_count})
   
    # Return the updated visit count
-    return {"statusCode": 200, "body": str(visit_count)} or {"statusCode": 404, "body": "Item not found"}
-  
- 
+    return {"headers": {"Access-Control-Allow-Origin": "*"},"statusCode": 200, "body": str(visit_count)} or {"statusCode": 404, "body": "Item not found"}
